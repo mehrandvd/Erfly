d0282da8f4569d0db4f8302e48285b4d715e593f
Mehran Davoudi <mehran.davoudi@hotmail.com>
2018-11-17 01:21:46 +0330

    Just born, soul is alive now!

---
+++ src/Erfly/Erfly.App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+namespace Erfly.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = File.ReadAllText(@"Soul.csx");
+            CSharpScript.RunAsync(code);
+
+            Console.ReadLine();
+        }
+    }
+}
